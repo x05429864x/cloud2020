311ff6d28dba44b858140112472a1f4dfc9e4687
xqr <x0542986x@sina.com>
2020-07-27 18:11:09 +0800

    springcloud bus消息总线

--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -2,13 +2,13 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-@EnableConfigServer
-public class ConfigCenterMain3344 {
+public class ConfigClientMain3366 {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenterMain3344 .class,args);
+        SpringApplication.run( ConfigClientMain3366.class,args);
     }
 }
 
